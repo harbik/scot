9e03c4203f8a93d2de40ce0d4b308c702eb15f06
Gerard Harbers <harbers@gmail.com>
2022-01-17 14:17:33 -0800

    cargo troubles...

--- examples/difference/main.rs
+++ examples/difference/main.rs
@@ -1,10 +1,11 @@
 use rcs::differences::CieDE2000;
 use rcs::observers::CieObs1931;
-use rcs_testcharts::CheckerBabel;
+//use rcs_testcharts::CheckerBabel;
 use rcs::illuminants::D50;
 use rcs::models::CieLab;
+use rcs::swatches::Gray;
 
 pub fn main() {
-    let lab_babel : CieLab<D50, CieObs1931> = CheckerBabel.into();
+    let lab_babel : CieLab<D50, CieObs1931> = Gray::<50>.into();
  //   let de = CieDE2000::<D65, CieObs1931>::new(CheckerBabel, CheckerBabel);
 }
