d63539c502e5108e5538fbf44a33af9f105f7879
Gerard Harbers <harbers@gmail.com>
2022-01-17 13:55:07 -0800

    rcs-testcharts > 0.0.1

--- examples/difference/main.rs
+++ examples/difference/main.rs
@@ -1,6 +1,10 @@
 use rcs::differences::CieDE2000;
 use rcs::observers::CieObs1931;
-use rcs_te
-pub fn main() {
+use rcs_testcharts::CheckerBabel;
+use rcs::illuminants::D50;
+use rcs::models::CieLab;
 
+pub fn main() {
+    let lab_babel : CieLab<D50, CieObs1931> = CheckerBabel.into();
+ //   let de = CieDE2000::<D65, CieObs1931>::new(CheckerBabel, CheckerBabel);
 }
